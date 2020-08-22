1c8f55ad779fc8066c1e33c9ae3c598fb4f1d874
Yago Lopes <yagolopeslazaro@gmail.com>
2020-08-22 13:46:57 -0300

    add ui kit chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Heading, Grid, Flex, Input } from "@chakra-ui/core";
+import logo from "./assets/logo.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid
+      as="main"
+      height="100vh"
+      templateColumns="1fr  480px 480px 1fr"
+      templateRows="1fr 480px 1fr"
+      templateAreas="
+        '. . . .'
+        '. logo form .'
+        '. . . .'
+      "
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Flex gridArea="logo" flexDir="column" alignItems="flex-start">
+        <img src={logo} alt="Rocketseat" />
+        <Heading size="2xl" lineHeight="shorter" marginTop="16">
+          Faça seu login na plataforma
+        </Heading>
+      </Flex>
+      <Flex
+        gridArea="form"
+        height="100%"
+        backgroundColor="gray-700"
+        borderRadius="md"
+        flexDir="column"
+        alignItems="stretch"
+        padding={16}
+      >
+        <Input
+          height="50px"
+          backgroundColor="gray.800"
+          focusBorderColor="purple-500"
+          borderRadius="sm"
+          placeholder="E-mail"
+        />
+      </Flex>
+    </Grid>
   );
 }
 
