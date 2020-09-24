9531356257ece508590348e985581fb0af735eea
Yago Lopes <yagolopeslazaro@gmail.com>
2020-09-24 17:09:19 -0300

    Updates

---
+++ precache-manifest.e2fcfd9d307e9cabbbda2713e4609319.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33c351331d69535682230b6b7faf5a06",
+    "url": "/ui-declarative/index.html"
+  },
+  {
+    "revision": "4e8cc1aefcbe77a4e207",
+    "url": "/ui-declarative/static/js/2.2cfe46f6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ui-declarative/static/js/2.2cfe46f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "285fa5d6340f888d7a1c",
+    "url": "/ui-declarative/static/js/main.8507a64a.chunk.js"
+  },
+  {
+    "revision": "5ad626d05890f7b51c54",
+    "url": "/ui-declarative/static/js/runtime-main.21820c64.js"
+  },
+  {
+    "revision": "697f9abe771ede5cae60ba51aa4ff25a",
+    "url": "/ui-declarative/static/media/logo.697f9abe.png"
+  }
+]);
